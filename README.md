7a357b86b5b1eab16d61ec4019c5142aecc325a9
mohamednayef <mohamednayef2002@gmail.com>
2023-11-08 15:46:25 +0200

    add test file

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # algorithm
-some algorithm codes
+some algorithm codes in Science Colleage on 7 Nov 2023 Level 3 term 1
---
+++ testtestest.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "hello from test\n";
+
+    return 0;
+}

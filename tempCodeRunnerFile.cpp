5880999badb1087f84e6dfd990da414d83266986
mohamednayef <mohamednayef2002@gmail.com>
2023-11-08 23:33:26 +0200

    new changes

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+void print_size_of_array(int arr[]){
+//     cout << "the size of array is=>" << sizeof(arr) / sizeof(arr[0]) << endl;
+//     return;
+// }
--- testtestest.cpp
+++ testtestest.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 using namespace std;
+
+void print_size_of_array(int arr[]);
+
+
 int main(){
-    long long n;
-    cin >> n;
-    cout << 2 * ((n*(n+1))/2) << endl;
+    int arr[] = {11,22,33,44,55};
+    // print_size_of_array(arr);
+    cout << "the size of array is=>" << sizeof(arr) / sizeof(arr[0]) << endl;
     return 0;
 }
+
+// void print_size_of_array(int arr[]){
+    // cout << "the size of array is=>" << sizeof(arr) / sizeof(arr[0]) << endl;
+//     return;
+// }

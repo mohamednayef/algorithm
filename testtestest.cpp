5fd01612e87303f319b68c303b9bd694dbc1b367
mohamednayef <mohamednayef2002@gmail.com>
2023-11-09 11:15:04 +0200

    wow

--- testtestest.cpp
+++ testtestest.cpp
@@ -1,17 +1,8 @@
 #include <iostream>
 using namespace std;
 
-void print_size_of_array(int arr[]);
-
-
 int main(){
-    int arr[] = {11,22,33,44,55};
-    // print_size_of_array(arr);
-    cout << "the size of array is=>" << sizeof(arr) / sizeof(arr[0]) << endl;
+    cout << "Hello from test.cpp 9 Nov 2023 @11:14\n";
+        
     return 0;
 }
-
-// void print_size_of_array(int arr[]){
-    // cout << "the size of array is=>" << sizeof(arr) / sizeof(arr[0]) << endl;
-//     return;
-// }

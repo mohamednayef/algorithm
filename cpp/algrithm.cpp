bd2b162f3049fd3e110bf2c03e60499d5a191abd
mohamednayef <mohamednayef2002@gmail.com>
2023-12-05 17:33:40 +0200

    w

--- cpp/algrithm.cpp
+++ cpp/algrithm.cpp
@@ -233,20 +233,20 @@ void quick_sort(int arr[], int n);
 
 void merge_sort(int arr[], int n);
 
-void sort_insertion(int arr[], int size){
-    for(int i=0; i<size-1; i++){
-        for(int j=i+1; j>0; j--){
-            if(arr[j] < arr[j-1]){
-                int tmp = arr[j];
-                arr[j] = arr[j-1];
-                arr[j-1] = tmp;
-            }
-            else{
-                break;
-            }
-        }
-    }
-}
+// void sort_insertion(int arr[], int size){
+//     for(int i=0; i<size-1; i++){
+//         for(int j=i+1; j>0; j--){
+//             if(arr[j] < arr[j-1]){
+//                 int tmp = arr[j];
+//                 arr[j] = arr[j-1];
+//                 arr[j-1] = tmp;
+//             }
+//             else{
+//                 break;
+//             }
+//         }
+//     }
+// }
 
 void sort_insertion(int arr[], int size){
     for(int i=0; i<size; i++){
--- cpp/main.cpp
+++ cpp/main.cpp
@@ -3,7 +3,11 @@
 using namespace std;
 
 int main(){
-  print();
+  int arr[5] = {33,22,55,11,44};
+
+  sort_bubble(arr,5);
+
+  display_arr(arr,5);
 
 
   return 0;
